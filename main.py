4443feb2d625158b385c37240a2ad7ead24c8432
Yash Desai <yddc902@live.com>
2020-05-29 18:44:38 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+ # def greet(*args):
+# 	return "Hello, "+args[3]
+
+
+# print(greet("Yash", 26, "Desai", "Bharuch"))
+
+# def greet(fname, lname, age, city):
+# 	print(f"My name is {fname} {lname}. I am {age} years old. I live in {city}.")
+# greet(lname="Thakor", fname="Nikunj", city="Bharuch", age=26)
+
+
+# arbitary -> numbers of..   *args
+# keyword => orders of parametes  kwd
+
+# arbitary keyword arg  =>  **kwargs
+
+
+# def greet(**args):
+# 	print(f"My name is {args['fname']} {args['lname']}. I am {args['age']} years old.")
+
+# greet(fname = "Yash", age=26, lname="Desai")
+
+# import mymodule as mm
+
+# m = mm.Hello()
+
+# print(m.greet("Yash"))
+
+
+
+#howdoi
